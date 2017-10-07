23a253509fe0ff8f92675abf87e51b7d8aa771d7
semuadmin <semuadmin@users.noreply.github.com>
2017-10-07 20:11:23 +0100

    Changed time to timer
    Change time to timer to avoid compilation error on Arduino Due

--- examples/ds4_i2c_test/ds4_i2c_test.ino
+++ examples/ds4_i2c_test/ds4_i2c_test.ino
@@ -34,8 +34,9 @@
 */
 
 #include <DS4_I2C_CONTROL.h>
+#define DEBUG false
 
-unsigned long time;
+unsigned long timer;
 uint32_t baudRate = 115200;
 uint8_t read_interval = 20;
 DS4_I2C_CONTROL ds4 = DS4_I2C_CONTROL(0x29);
@@ -43,16 +44,19 @@ DS4_I2C_CONTROL ds4 = DS4_I2C_CONTROL(0x29);
 void setup()  {
   while( !Serial ); // sometimes necessary with Teensy 3 or Arduino Micro
   Serial.begin(baudRate);
-  time = 0;
+  timer = 0;
   ds4.begin();
 }
 
 void loop()  {
 
-  if (millis() > time)
+  if (millis() > timer)
   {
-    time = millis() + read_interval;
+    timer = millis() + read_interval;
     ds4.get_ps4();
+    if (DEBUG) {
+       Serial.println((String)"ps4_ok = " + ds4.ps4_ok);
+    }   
     if (ds4.ps4_ok)
     {
       showStatus();
